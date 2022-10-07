c5a0a74d97f3b268dccd16709726c9f26439a3b3
David <davidongaroh@gmail.com>
2022-10-07 15:51:27 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_124038) do
+ActiveRecord::Schema.define(version: 2022_10_07_125009) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_10_07_124038) do
     t.integer "minutes_to_complete"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
