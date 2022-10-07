546b13d5292d1e5595935d15ffb649258fdd5fdd
David <davidongaroh@gmail.com>
2022-10-07 15:44:12 +0300

    add validations and migrate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :description, length: {minimum: 50}
 end
