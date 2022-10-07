c9a2da6169a11bded8e4f1999ab629a9742c9141
David <davidongaroh@gmail.com>
2022-10-07 16:04:33 +0300

    add /signup & #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+
+    def create
+        users = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        end
+    end
+
+
+    private
+    def render_invalid(invalid)
+        render json:{error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
